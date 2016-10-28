732d5777f5f8b1f2a62ee7c22a82985243a13e06
zztong <494767964@qq.com>
2016-10-28 11:15:08 +0800

    react two

---
+++ react/app/index.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    console.log(11111);
+    var date = new Date();
+    console.log(date);
+    window.setInterval(function(){
+        alert(2222222222);
+    },5000)
+}
